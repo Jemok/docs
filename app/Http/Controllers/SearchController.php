ecefc42d3cd6d44003a310afe051c7dc2534f1ee
James Mwangi <karokijames40@gmail.com>
2016-07-29 02:26:03 +0300

    Adds user search logic

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -2,15 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Repositories\ClassRepository;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\Http\Requests\SearchRequest;
+use App\User;
+use Illuminate\Support\Facades\Auth;
 
 class SearchController extends Controller
 {
 
-    public function search(){
+    public function search(SearchRequest $searchRequest, ClassRepository $classRepository){
 
-        return view('search.search_results');
+        $query = $searchRequest->search;
+
+        if($query){
+
+            $users = User::where('name', 'LIKE', "%$query%")
+                ->where('account_type', 0)
+                ->orWhere('email', 'LIKE', "%$query%")
+                ->paginate(10);
+
+            foreach ($users as $user){
+
+                $user_class = $user->intake()->with('year', 'month', 'course', 'division')->first();
+
+                $class_name[] = $classRepository->makeName($user_class);
+            }
+
+        }
+
+        return view('search.search_results', compact('users', 'class_name'));
     }
 }
--- app/Http/Requests/SearchRequest.php
+++ app/Http/Requests/SearchRequest.php
@@ -13,7 +13,7 @@ class SearchRequest extends Request
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,8 @@ class SearchRequest extends Request
     public function rules()
     {
         return [
-            //
+
+            'search' => 'required'
         ];
     }
 }
