ecfe75fef598c9c6e388b85038dae9225c7fc343
renncomp58 <renncomp58@outlook.com>
2016-08-01 10:59:43 +0300

    landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,12 +3,24 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
+        <div class="col-md-3">
+            <div class="panel panel-default panel-left">
+                <div class="panel-heading">
+                    <h5><strong>About Docs</strong></h5>
+                </div>
+                <div class="panel-body">
+
+                </div>
+            </div>
+
+        </div>
+
+        <div class="col-md-8 col-md-offset-1">
+            <div class="panel panel-default panel-right">
                 <div class="panel-heading">Welcome</div>
 
                 <div class="panel-body">
-                    Your Application's Landing Page.
+                    <img src="{{"images/open-book-1690.jpg"}}" alt="open-book" class="index-image"/>
                 </div>
             </div>
         </div>
