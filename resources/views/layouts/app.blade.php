8b7d3d9a271b1a7b417f0df26c6f856afdabecfa
James Mwangi <karokijames40@gmail.com>
2016-08-09 16:10:19 +0300

    Adds changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Docs</title>
+    <title>Students Lecturer File Sharing System</title>
 
     <!-- Fonts -->
     <link href="{{ asset('css/bootstrap_fonts') }}" type="text/css" rel="stylesheet">
